a0372895988b48f025b9b262acf42698dc0c1bb9
Prabu <prabuisthebest@gmail.com>
2019-03-30 10:22:43 +0530

    heroku up

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ivgz2rnl5rh7sphb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'v7pyk5hz66gcyot2'),
+            'username' => env('DB_USERNAME', 'bi50o5kwe3lrcznj'),
+            'password' => env('DB_PASSWORD', 'o0ft053wnanbke19'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
